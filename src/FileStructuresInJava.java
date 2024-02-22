ec56117ce6b015a9aad5bee92094b63c3cc2e051
DILIP KUMAR BC <dilipkumarbc8055@gmail.com>
2024-02-22 12:48:23 +0530

    File Structure in java

---
+++ src/FileStructuresInJava.java
@@ -0,0 +1,17 @@
+//we can hava any number of class in java
+//only on public class should be there in a java file 
+//the file name and class name should be the same 
+// we can have any number of main function in java file 
+//if the java file is not having any public class then we can name the file as user wish 
+
+public class FileStructuresInJava {
+
+}
+
+class A {
+
+}
+
+class B {
+
+}
